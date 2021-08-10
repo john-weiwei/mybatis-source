bc3bdf8cb37f208f2095a0ec265095d248981358
allen's-qfang <6300@qfang.com>
2021-08-10 09:20:48 +0800

    fix:创建dev分支

--- src/main/java/com/cn/MybatisApplication.java
+++ src/main/java/com/cn/MybatisApplication.java
@@ -7,6 +7,6 @@ package com.cn;
  */
 public class MybatisApplication {
     public static void main(String[] args) {
-        System.out.println("输出内容");
+        System.out.println("输出内容dev");
     }
 }
